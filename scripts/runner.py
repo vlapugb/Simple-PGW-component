a1c832823a22266bdc42bb93d794255f3ae0cf6b
vlapugb <vladimirpugovkinspb@gmail.com>
2025-07-23 02:37:54 +0300

    fix: make -> ninja and add cashing to build.yaml

--- scripts/runner.py
+++ scripts/runner.py
@@ -6,6 +6,10 @@ projdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 os.makedirs(f"{projdir}/build", exist_ok=True)
 os.chdir(f"{projdir}/build")
 build_type = sys.argv[1] if len(sys.argv) > 1 else "Debug"
-subprocess.run(["cmake", f"-DCMAKE_BUILD_TYPE={build_type}", ".."], check=True)
-subprocess.run(["cmake", "--build", ".", "--", f"-j{os.cpu_count()}"], check=True)
+subprocess.run(["cmake", "-G", "Ninja",
+                f"-DCMAKE_BUILD_TYPE={build_type}",
+                "-DCMAKE_C_COMPILER_LAUNCHER=ccache",
+                "-DCMAKE_CXX_COMPILER_LAUNCHER=ccache",
+                ".."], check=True)
+subprocess.run(["cmake", "--build", ".", "--parallel"], check=True)
 subprocess.run(["ctest", "--output-on-failure"], check=True)
