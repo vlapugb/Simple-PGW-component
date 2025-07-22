1be605b1b0aab1eb15aa164d4513e30e6f70a63e
vlapugb <vladimirpugovkinspb@gmail.com>
2025-07-22 22:45:26 +0300

    add: added basic runner.py script to run build

---
+++ scripts/runner.py
@@ -0,0 +1,11 @@
+import subprocess
+import os
+import sys
+
+projdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+os.makedirs(f"{projdir}/build", exist_ok=True)
+os.chdir(f"{projdir}/build")
+build_type = sys.argv[1] if len(sys.argv) > 1 else "Debug"
+subprocess.run(["cmake", f"-DCMAKE_BUILD_TYPE={build_type}", ".."], check=True)
+subprocess.run(["cmake", "--build", ".", "--", f"-j{os.cpu_count()}"], check=True)
+subprocess.run(["ctest", "--output-on-failure"], check=True)
