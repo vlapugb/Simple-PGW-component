2101467f2ffce47c79ee5bd676894e43816601dc
vlapugb <vladimirpugovkinspb@gmail.com>
2025-07-23 14:16:46 +0300

    fix: deleted ccache depend in runner.py

--- scripts/runner.py
+++ scripts/runner.py
@@ -8,8 +8,6 @@ os.chdir(f"{projdir}/build")
 build_type = sys.argv[1] if len(sys.argv) > 1 else "Debug"
 subprocess.run(["cmake", "-G", "Ninja",
                 f"-DCMAKE_BUILD_TYPE={build_type}",
-                "-DCMAKE_C_COMPILER_LAUNCHER=ccache",
-                "-DCMAKE_CXX_COMPILER_LAUNCHER=ccache",
                 ".."], check=True)
 subprocess.run(["cmake", "--build", ".", "--parallel"], check=True)
 subprocess.run(["ctest", "--output-on-failure"], check=True)
