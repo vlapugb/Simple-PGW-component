2c3c4d82544b702814085071769fbe7583289f0f
vlapugb <vladimirpugovkinspb@gmail.com>
2025-07-27 15:18:06 +0300

    add: added SessionManager component

--- src/Pgw/SessionManager.h
+++ src/Pgw/SessionManager.h
@@ -1,9 +1,37 @@
+#pragma once
+#include "CdrWriter.h"
+#include <chrono>
+#include <mutex>
+#include <string>
+#include <unordered_map>
 
-//
-// Created by vkinzin on 7/22/25.
-//
 
-#ifndef SESSIONMANAGER_H
-#define SESSIONMANAGER_H
+class CdrWriter;
 
-#endif //SESSIONMANAGER_H
+class SessionManager {
+    public:
+    explicit SessionManager (size_t timeout_sec, CdrWriter& writer, const std::shared_ptr<spdlog::logger>& logger);
+    ~SessionManager () = default;
+
+    bool create_session (const std::string& imsi);
+
+    bool has_session (const std::string& imsi) const;
+
+    void remove_timeout ();
+
+    bool remove_session (const std::string& imsi, std::string_view reason);
+
+    size_t session_count () const;
+
+    bool pop_one (std::string& imsi_out);
+
+    private:
+    struct SessionInfo {
+        std::chrono::steady_clock::time_point start_time;
+    };
+    size_t _timeout_sec = 0;
+    mutable std::mutex _mutex;
+    std::unordered_map<std::string, SessionInfo> _sessions;
+    CdrWriter& _cdr_writer;
+    std::shared_ptr<spdlog::logger> _logger;
+};
